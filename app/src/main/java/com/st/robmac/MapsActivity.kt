e42b25dedc858bf4a57868a46f84aabe07fef61b
m9 <paulbravomacedo@gmail.com>
2022-10-26 03:03:59 -0500

    Anadido Mapa

---
+++ app/src/main/java/com/st/robmac/MapsActivity.kt
@@ -0,0 +1,37 @@
+package com.st.robmac
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+private lateinit var map: GoogleMap
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        createMapFragment()
+    }
+
+    private fun createMapFragment() {
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentMap) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        createMarker()
+    }
+
+    private fun createMarker() {
+        val coordinates = LatLng(-11.961275357326729, -77.06568023093887)
+        val marker: MarkerOptions =
+            MarkerOptions().position(coordinates).title("UCH")
+        map.addMarker(marker)
+    }
+}
