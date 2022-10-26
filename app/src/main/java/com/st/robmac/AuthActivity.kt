30e606f2eaadff086d39f0a922442a15c9c3f3ac
m9 <paulbravomacedo@gmail.com>
2022-10-25 22:41:02 -0500

    Se agrego logica de registro y notificacion de error.

--- app/src/main/java/com/st/robmac/AuthActivity.kt
+++ app/src/main/java/com/st/robmac/AuthActivity.kt
@@ -2,10 +2,47 @@ package com.st.robmac
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_auth.*
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+
+        setup()
     }
+
+    private fun setup() {
+        title = "Autenticacion"
+        signUp.setOnClickListener {
+            if (ETemail.text.isNotEmpty() && ETpassword.text.isNotEmpty()) {
+                FirebaseAuth.getInstance()
+                    .createUserWithEmailAndPassword(
+                        ETemail.text.toString(),
+                        ETpassword.text.toString()
+                    ).addOnCompleteListener {
+                        if (it.isSuccessful) {
+
+                        } else {
+                            showAlert()
+                        }
+                    }
+            }
+        }
+    }
+
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error autenticando al usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+   // private fun showHome(email: String, provider: ProviderType)
+
+
 }
